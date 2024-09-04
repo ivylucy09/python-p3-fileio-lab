1980a2aaa98517df79ca78a2b845963862c6ac4a
Ivylucyawuor <awuorivy09>
2024-09-04 15:43:34 +0300

    fileio

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,22 @@
+# lib/file_io.py
+
 def write_file(file_name, file_content):
-    pass
+    # Convert file_name to a string and add the .txt extension
+    with open(str(file_name) + ".txt", "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    # Convert file_name to a string and add the .txt extension
+    with open(str(file_name) + ".txt", "a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    # Convert file_name to a string and add the .txt extension
+    with open(str(file_name) + ".txt", "r") as file:
+        return file.read()
+
+# use write_file function. 
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added" )
+append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
+
+read_file(file_name="logfile")
